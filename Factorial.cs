cda9a4ca3354e850ed65fcd5b966b8e856444e3f
itishree2 <nayakitishree6262@gmail.com>
2024-02-15 15:02:56 +0530

    first

---
+++ Factorial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAPP_Assessment2
+{
+    public class Factorial
+    {
+        static void Main(string[] args)
+         {
+                int fact = 1;
+                for (int i = 1; i < 10; i++)
+                {
+                    fact = fact * i;
+                }
+                Console.WriteLine("Factorial of 10 is:" + fact);
+
+            }
+
+
+        }
+    }
+
